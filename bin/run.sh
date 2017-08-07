6d724cb382acc5a8cd3af890a6a0078cd355c6ff
Sayat Satybaldiyev <stb@satybald.com>
2017-08-07 21:35:53 +0200

    Fix run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,11 @@
+echo "Building Image"
+VERSION=0.1
+IMAGE=presto-docker:$VERSION
+docker build -t $IMAGE .
+
 docker run -it \
 -e IS_COORDINATOR=true \
+-e IS_DISCOVERY_ENABLED=false \
 -e DISCOVERY_URL=http://192.168.1.3:8081 \
 --net=host \
-presto-docker
+$IMAGE
